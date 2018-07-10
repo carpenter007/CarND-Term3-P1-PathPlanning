4fdadee8cde55ee05fa44c935cfa224b2f445014
Unknown <elias@zimi.de>
2018-07-11 00:56:59 +0200

    get the ego-car loop one round with only few collisions and less jerk

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -200,7 +201,10 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  int lane_number = 1; // lane number: 0 = left lane, 1 = middle lane, 2 = right lane
+  float v_dest = 0; // v_dest is the velocity we want to reach
+
+  h.onMessage([&v_dest, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane_number](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -218,7 +222,7 @@ int main() {
         
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
         	// Main car's localization Data
           	double car_x = j[1]["x"];
           	double car_y = j[1]["y"];
@@ -239,13 +243,175 @@ int main() {
 
           	json msgJson;
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
+          	int prev_size = previous_path_x.size();
+          	bool too_close = false;
+          	/***********************************************************************************************************
+          	 * User code start
+          	 *
+          	 * define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+          	 ***********************************************************************************************************/
 
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+          	// setup variables
+
+            vector<double> anchors_x; // x anchor values which will be used to calculate a spline over a long distance
+            vector<double> anchors_y; // y anchor values which will be used to calculate a spline over a long distance
+            double ref_x = car_x; // store current cars x position, later we use this variable for the last calculated path x point
+            double ref_y = car_y; // store current cars x position, later we use this variable for the last calculated path y point
+            double ref_yaw = deg2rad(car_yaw); // store current cars angel
+            float v_dest_max = 49.5;
+
+            // Check the sensor fusion data
+
+            for(int i = 0; i < sensor_fusion.size(); i++)
+            {
+              double d = sensor_fusion[i][6u];
+              // Is the car on our lane?
+              if( ((4*lane_number) < d) && ((4+4*lane_number) >= d) )
+              {
+                // What is the distance of the car (after our path is passed)
+                double vx = sensor_fusion[i][3u];
+                double vy = sensor_fusion[i][4u];
+                double velocity_car = sqrt(vx*vx + vy*vy);
+                double distance = sensor_fusion[i][5u];
+                distance = (distance + (0.02 * (double)prev_size * velocity_car / 2.24)) - end_path_s;
+
+                if(distance >= 0.0 && distance <= 30.0) /* caution on wrap after highway is completed */
+                {
+                  too_close = true;
+                  v_dest_max = velocity_car;
+                  if(lane_number > 0)
+                  {
+                    lane_number--;
+                  }
+                  else
+                  {
+                    lane_number++;
+                  }
+                }
+              }
+            }
+
+            if(too_close && (v_dest > v_dest_max))
+            {
+              v_dest -= 0.224;
+            }
+            else if(v_dest < 49.5)
+            {
+              v_dest += 0.224;
+            }
+            // What is the velocity of the car
+
+            // Penalty these issues
+
+            // Predict the other cars in the future
+            // Give some possible trajectories (faster ones, slower ones, w/wo lane changes, early lane changes, later lane changes
+            // apply some cost functions on each trajectory and penalty it with a given weight
+            // cost functions: the closer (distance) to other vehicles the higher the costs,
+            // get the best trajectory and apply it
+
+
+            // set the first two anchor points.
+            if(prev_size < 2)
+            {
+              // if there is no path calculated, we use the current and the previous position as an anchor point
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+              anchors_x.push_back(prev_car_x);
+              anchors_x.push_back(ref_x);
+              anchors_y.push_back(prev_car_y);
+              anchors_y.push_back(ref_y);
+            }
+            else
+            {
+              // if there is still a previous path (with more than two update values (x,y) left), append the last calculated points as anchor points
+              ref_x = previous_path_x[prev_size - 1];
+              ref_y = previous_path_y[prev_size - 1];
+              double ref_x_prev = previous_path_x[prev_size - 2];
+              double ref_y_prev = previous_path_y[prev_size - 2];
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+              anchors_x.push_back(ref_x_prev);
+              anchors_x.push_back(ref_x);
+              anchors_y.push_back(ref_y_prev);
+              anchors_y.push_back(ref_y);
+            }
+
+
+            // Setup further anchor points in a distance of 30, 60 and 90 meters
+          	vector<double> next_wp0 = getXY(car_s + 50, (2+4*lane_number), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          	vector<double> next_wp1 = getXY(car_s + 100, (2+4*lane_number), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          	vector<double> next_wp2 = getXY(car_s + 150, (2+4*lane_number), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          	anchors_x.push_back(next_wp0[0]);
+          	anchors_x.push_back(next_wp1[0]);
+          	anchors_x.push_back(next_wp2[0]);
+          	anchors_y.push_back(next_wp0[1]);
+            anchors_y.push_back(next_wp1[1]);
+            anchors_y.push_back(next_wp2[1]);
+
+
+            // Shift car reference angle to 0 degrees
+            for(int i = 0; i < anchors_x.size(); i++ )
+            {
+              double shift_x = anchors_x[i] - ref_x;
+              double shift_y = anchors_y[i] - ref_y;
+
+              // Avoid to get the spline to get vertical. Shift the with the current global map angle to make sure the spline goes in x direction
+              anchors_x[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
+              anchors_y[i] = (shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw));
+            }
+
+
+
+
+
+            vector<double> next_x_vals;
+            vector<double> next_y_vals;
+
+            // reusing previous path points avoids jerk by smoothly incept new trajectories step by step to the end of the previously calculated trajectory
+            for(int i = 0; i < prev_size; i++)
+            {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            // Define a spline with the set up anchor points, using the spline library
+            tk::spline s;
+            s.set_points(anchors_x, anchors_y);
+            // The spline will be linearized within a distance of 30 meters. Depending on the v_dest, the number of path points over this distance will be calculated
+
+            double target_x = 30.0;
+            double target_y = s(target_x);
+            double target_dist = sqrt((target_x)*(target_x)+ (target_y)*(target_y));
+            double x_add = 0.0;
+
+
+
+            for(int i = 0; i < 50 - prev_size; i++)
+            {
+                double N = (target_dist / (0.02 * v_dest / 2.24));
+                double x_point = x_add + (target_x)/N;
+                double y_point = s(x_point);
+
+                x_add = x_point;
+
+                double x_ref = x_point;
+                double y_ref = y_point;
+
+                // Shift back to the origin global map angle
+                x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+                y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+                x_point += ref_x;
+                y_point += ref_y;
+
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
+            }
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
+
+
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
